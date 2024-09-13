c94d736d9127991ccd7bd71011a150c5a1fe05ff
lunqwe <roma.boyko10068@gmail.com>
2024-09-13 21:02:25 +0300

    refactor + added login/logout functionalities

--- api/accounts/routes.py
+++ api/accounts/routes.py
@@ -1,9 +1,12 @@
 from typing import Annotated
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, HTTPException
 from sqlalchemy.orm import Session
+from fastapi.responses import JSONResponse
+from sqlalchemy.exc import IntegrityError
+from jwt.exceptions import InvalidTokenError
 
 from config import get_db
-from .schemas import UserBase, UserRegister, RefreshToken
+from .schemas import UserBase, UserRegister, RefreshToken, UserLogin
 from .services import UserService, JWTService
 from .models import User
 
@@ -16,21 +19,35 @@ jwt_service = JWTService()
 async def register(request: UserRegister, db: Session = Depends(get_db)):
     try:
         data = user_service.register_user(data=request, db=db)
-        return data
+        return JSONResponse(content=data, status_code=status.HTTP_201_CREATED)
     except ValueError as e:
-        return {'detail': e.args[0]}
+        return HTTPException(status_code=401, details=e.args[0])
+    except IntegrityError as e:
+        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, details=e.args[0])
 
 
 @app.post('/refresh')
 async def refresh_token(request: RefreshToken, db: Session = Depends(get_db)):
+    jwt_service.check_refresh_expired(db, request.refresh)
     access_token = jwt_service.create_access_token(db, refresh_token=request.refresh)
-    return {'access': access_token}
+    return JSONResponse({'access': access_token}, status_code=status.HTTP_201_CREATED)
+
+
+@app.post('/login')
+async def login(data: UserLogin, db: Session = Depends(get_db)):
+    response_data = user_service.login_user(data, db)
+    return JSONResponse(response_data, status_code=status.HTTP_200_OK)
+
+
+@app.post('/logout')
+async def logout(current_user: Annotated[User, Depends(jwt_service.get_current_user)], db: Session = Depends(get_db)):
+    response_data = user_service.logout(user=current_user, db=db)
+    return JSONResponse(response_data, status_code=status.HTTP_200_OK)
+    
 
 # jwt tokens test
-
-
 @app.get("/me", response_model=UserBase)
 async def read_users_me(
     current_user: Annotated[User, Depends(jwt_service.get_current_user)],
 ):
-    return current_user
+    return JSONResponse(UserBase(**current_user.__dict__).__dict__, status_code=status.HTTP_200_OK)
--- api/accounts/services.py
+++ api/accounts/services.py
@@ -7,11 +7,12 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.sql import exists
 from psycopg2.errors import UniqueViolation
 
 import config
 from .models import User, RefreshToken
-from .schemas import UserInDB, UserRegister, UserRefreshTokenData, UserBase
+from .schemas import UserInDB, UserLogin, UserRegister, UserRefreshTokenData, UserBase
 
 
 class JWTService:
@@ -29,54 +30,100 @@ class JWTService:
     def encrypt_password(self, password: str):
         return self.pwd_context.hash(password)
 
-    def encode_data(self, data: dict, token_type: str):
-        if token_type:
-            if token_type == 'access':
-                expire = datetime.now(timezone.utc) + timedelta(
-                    minutes=config.ACCESS_TOKEN_EXPIRES_MINUTES)
-                to_encode = data.copy()
-            elif token_type == 'refresh':
-                expire = datetime.now(timezone.utc) + timedelta(
-                    days=config.REFRESH_TOKEN_EXPIRES_DAYS)
-                to_encode = {'username': data.username, 'email': data.email}
+    def check_user_tokens(self, user):
+        if len(user.tokens) > 0:
+            return True
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail='Credentials error: Token expired.'
+                                )
+    
+    def check_refresh_expired(self, db: Session, token: str):
+        token_in_db = db.query(RefreshToken).filter(RefreshToken.token == token).first()
+        if token_in_db:
+            if token_in_db.expires_at.tzinfo is None:
+                token_in_db.expires_at = token_in_db.expires_at.replace(tzinfo=timezone.utc)
+            if datetime.now(timezone.utc) > token_in_db.expires_at:
+                db.delete(token_in_db)
+                db.commit()
+                raise HTTPException(
+                    status_code=status.HTTP_403_FORBIDDEN,
+                    detail='Credentials error: Token expired.')
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail='Credentials error: Token expired.')
+    
+    def encode_data(self, data: dict, token_type: str) -> str:
+        expire = datetime.now(timezone.utc) + (
+            timedelta(minutes=config.ACCESS_TOKEN_EXPIRES_MINUTES) 
+            if token_type == 'access' else 
+            timedelta(days=config.REFRESH_TOKEN_EXPIRES_DAYS)
+        )
+        
+        to_encode = {'exp': expire, **data}  # Access токен будет содержать только необходимую информацию
+        encoded_jwt = jwt.encode(to_encode, config.SECRET_KEY, algorithm=config.ALGORITHM)
+        return encoded_jwt
 
-            to_encode.update({'exp': expire})
-            encoded_jwt = jwt.encode(to_encode,
-                                     config.SECRET_KEY, algorithm=config.ALGORITHM)
-            return encoded_jwt
-
-    def create_refresh_token(self, db: Session, data: dict):
+    def create_refresh_token(self, db: Session, data: UserRefreshTokenData) -> RefreshToken:
         user = self.get_user(db, username=data.username)
-        token = self.encode_data(data, token_type='refresh')
+        if not user:
+            raise ValueError("User not found.")
+        
+        token_data = {'sub': user.id}
+        token = self.encode_data(token_data, token_type='refresh')
+        
         db_token = RefreshToken(user=user, user_id=user.id, token=token)
         db.add(db_token)
+        db.commit()
         return db_token
 
-    def create_access_token(self, refresh_token: str):
-        data = jwt.decode(refresh_token, config.SECRET_KEY, algorithms=[config.ALGORITHM])
-        if data.get('username'):
-            token = self.encode_data(data, token_type='access')
-            return token
+    def create_access_token(self, db: Session, refresh_token: str) -> str:
+        try:
+            data = jwt.decode(refresh_token, config.SECRET_KEY, algorithms=[config.ALGORITHM])
+            user_id = data.get('sub')
+            if user_id is None:
+                raise InvalidTokenError("Credentials error.")
+            
+            user = db.query(User).get(user_id)
+            if not user:
+                raise ValueError("User not found.")
+            
+            access_token_data = {'username': user.username}
+            return self.encode_data(access_token_data, token_type='access')
 
-    def get_current_user(self, token: Annotated[str, Depends(oauth2_scheme)],
-                         db: Session = Depends(config.get_db)):
+        except InvalidTokenError:
+            raise InvalidTokenError("Invalid refresh token.")
+
+    def get_current_user(self, token: Annotated[str, Depends(oauth2_scheme)], db: Session = Depends(config.get_db)):
         credentials_exception = HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Could not validate credentials",
             headers={"WWW-Authenticate": "Bearer"},
         )
+
         try:
             payload = jwt.decode(token, config.SECRET_KEY, algorithms=[config.ALGORITHM])
+            exp = payload.get('exp')
+            exp_datetime = datetime.fromtimestamp(exp, tz=timezone.utc)
+            if datetime.now(timezone.utc) > exp_datetime:
+                raise HTTPException(
+                    status_code=status.HTTP_403_FORBIDDEN,
+                    detail='Credentials error: Token expired.'
+                    )
             username = payload.get('username')
             if username is None:
                 raise credentials_exception
-            token_data = UserBase(username=username)
         except InvalidTokenError:
             raise credentials_exception
-        user = self.get_user(db, username=token_data.username)
+
+        user = self.get_user(db, username=username)
+        self.check_user_tokens(user)
         if user is None:
             raise credentials_exception
         return user
+
 
 
 class UserService:
@@ -102,21 +149,56 @@ class UserService:
             email=data.email,
             hashed_password=hashed_password,
         )
-
         try:
             user = self.create_user(db, user_data)
-            refresh_token = self.jwt_service.create_refresh_token(db, data=user_data)
-            access_token = self.jwt_service.create_access_token(db, refresh_token.token, data=user)
+            refresh_token = self.jwt_service.create_refresh_token(db, data=UserRefreshTokenData(**user.__dict__))
+            access_token = self.jwt_service.create_access_token(refresh_token.token)
+            print(access_token)
             db.commit()
             return {'refresh': refresh_token.token, 'access': access_token}
 
-        except IntegrityError:
+        except (IntegrityError, ValueError, InvalidTokenError):
             db.rollback()
-            raise ValueError("An error occurred during registration.")
-
-        except ValueError:
-            raise
+            raise 
 
         except Exception as e:
             db.rollback()
             raise ValueError(f"Unexpected error: {str(e)}")
+
+    def login_user(self, data: UserLogin, db: Session = Depends(config.get_db)):
+        password = data.password
+        user = self.jwt_service.get_user(db, username=data.username)
+        if user:
+            if self.jwt_service.verify_password(password, user.hashed_password):
+                try:
+                    refresh_token = self.jwt_service.create_refresh_token(db, data=user)
+                    access_token = self.jwt_service.create_access_token(db, refresh_token.token)
+                    db.commit()
+                except ValueError:
+                    db.rollback()
+                    raise
+                except Exception as e:
+                    print(e)
+                return {'refresh': refresh_token.token, 'access': access_token}
+            else:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail='Wrong password.'
+                    )
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f'Account with username {data.username} is not found'
+            )
+    
+    def logout(self, user, db: Session):
+        if len(user.tokens) > 0:
+            db.delete(user.tokens[0])
+            db.commit()
+            return {'detail': 'Logged out successfully!'}
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail=f'Could not validate credentials',
+            )
+        
